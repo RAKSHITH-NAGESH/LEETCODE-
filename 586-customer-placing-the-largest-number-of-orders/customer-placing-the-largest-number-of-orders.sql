3d0fe175903e7d0d7aa53f0856aad269a3c217fe
Rakshith Nagesh <rakshithntech@gmail.com>
2026-08-31 14:49:59 +0530

    Time: 525 ms (45.24%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,11 +1,5 @@
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
-HAVING COUNT(*) = (
-    SELECT MAX(cnt)
-    FROM (
-        SELECT COUNT(*) AS cnt
-        FROM Orders
-        GROUP BY customer_number
-    ) t
-);
+ORDER BY COUNT(*) DESC
+LIMIT 1;
